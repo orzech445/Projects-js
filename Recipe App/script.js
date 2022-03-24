d65f5773dec186edd0a3760e372666575581cddd
Janek <orzechowski445@gmail.com>
2022-03-24 21:59:40 +0100

    Update JS file

--- Recipe App/script.js
+++ Recipe App/script.js
@@ -1,3 +1,12 @@
-function getMeal() {
+const mealsContainer = document.getElementById("meals-container");
 
+getRandomMeal();
+
+async function getRandomMeal() {
+    const response = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
+
+    const responseData = await response.json();
+    const randomMeal = responseData.meals[0];
+
+    console.log(randomMeal);
 }
